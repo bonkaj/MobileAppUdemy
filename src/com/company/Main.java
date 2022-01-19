09ce9abf1dbbf6750b42e9ab32dc78dd48de24f2
Austin Bonk <austinbonk@Austins-MBP.localdomain>
2022-01-19 11:04:07 -0600

    added main input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,84 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    static MobilePhone mobilePhone = new MobilePhone("651 269 6499");
 
     public static void main(String[] args) {
 	// write your code here
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while(!quit){
+            System.out.println("\nEnter action: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action){
+                case 0:
+                    System.out.println("Shutting down.");
+                    quit = true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+            }
+        }
+    }
+
+    private static void addNewContact(){
+        System.out.println("Enter the contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter the phone number: ");
+        String phone = scanner.nextLine();
+        Contact newContact = Contact.createContact(name, phone);
+        if (mobilePhone.addNewContact(newContact)){
+            System.out.println("New contact added: "+ name + ", phone "+phone);
+        } else {
+            System.out.println("Cannot add, " + name + " already on file.");
+        }
+    }
+
+    private static void updateContact(){
+        System.out.println("Enter the old contact name: ");
+        String oldContact = scanner.nextLine();
+        System.out.println("Enter the new contact name: ");
+        String newContact = scanner.nextLine();
+        if (mobilePhone.updateContact(oldContact, newContact)){
+
+        }
+    }
+
+    private static void startPhone(){
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions(){
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0 - to shut down\n" +
+                            "1 - to print contacts\n" +
+                            "2 - to add a contact\n" +
+                            "3 - to update existing contact\n" +
+                            "4 - to remove an existing contact\n" +
+                            "5 - query if an existing contact exists\n" +
+                            "6 - print a list of available actions.");
+        System.out.println("Choose your action: ");
     }
 }
--- src/com/company/MobilePhone.java
+++ src/com/company/MobilePhone.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 
 public class MobilePhone {
 
-    ArrayList<Contacts> contact = new ArrayList<>();
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
 
-    String MobilePhone(String number){
-        this.contact = new ArrayList<Contacts>();
+    ArrayList<Contact> contact = new ArrayList<>();
+
+    public MobilePhone(String number){
+        this.myNumber = number;
+        this.myContacts = new ArrayList<Contact>();
     }
 
-    public boolean addNewContact(Contacts contacts){
+    public boolean addNewContact(Contact contact){
         if (findContact(contact.getName()) >= 0){
             System.out.println("Contact is already on file");
             return false;
@@ -20,7 +24,55 @@ public class MobilePhone {
         return true;
     }
 
-    private int findContact(Contacts contact) {
+    public boolean updateContact (Contact oldContact, Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if (foundPosition < 0){
+            System.out.println(oldContact.getName() + ", was not found!");
+            return false;
+        }
+        this.myContacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + " has been replaced with " + newContact.getName());
+        return true;
+    }
+
+    public boolean removeContact (Contact contact) {
+        int foundPosition = findContact(contact);
+        if (foundPosition < 0){
+            System.out.println(contact.getName() + ", was not found.");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName()+" was deleted.");
+        return true;
+    }
+
+    private int findContact(Contact contact) {
         return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String name){
+        for(int i = 0; i< this.contact.size(); i++){
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(name)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String queryContact (Contact contact){
+        if (findContact(contact) >= 0){
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public void printContacts(){
+        System.out.println("Contact List");
+        for (int i = 0; i < this.myContacts.size(); i++){
+            System.out.println((i + 1) + "."
+                    + this.myContacts.get(i).getName() + " -> "
+                    + this.myContacts.get(i).getNumber());
+        }
     }
 }
